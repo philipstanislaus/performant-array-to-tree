8b5212c8b4ce47982ffdaad6ea6b7a9172292310
Philip Stanislaus <6912756+philipstanislaus@users.noreply.github.com>
2019-08-12 14:30:26 +0200

    Add test for not existing parentId

--- build/arrayToTree.spec.js
+++ build/arrayToTree.spec.js
@@ -11,13 +11,17 @@ describe('arrayToTree', function () {
             { id: '1', parentId: '418', custom: 'ZZZz' },
             { id: '418', parentId: null, custom: 'ü' },
         ])).to.deep.equal([
-            { data: { id: '4', parentId: null, custom: 'abc' }, children: [
+            {
+                data: { id: '4', parentId: null, custom: 'abc' }, children: [
                     { data: { id: '31', parentId: '4', custom: '12' }, children: [] },
-                ] },
-            { data: { id: '418', parentId: null, custom: 'ü' }, children: [
+                ],
+            },
+            {
+                data: { id: '418', parentId: null, custom: 'ü' }, children: [
                     { data: { id: '1941', parentId: '418', custom: 'de' }, children: [] },
                     { data: { id: '1', parentId: '418', custom: 'ZZZz' }, children: [] },
-                ] },
+                ],
+            },
         ]);
     });
     it('should work with nested objects and custom keys', function () {
@@ -28,13 +32,39 @@ describe('arrayToTree', function () {
             { num: '1', ref: '418', custom: 'ZZZz' },
             { num: '418', ref: null, custom: 'ü' },
         ], { id: 'num', parentId: 'ref' })).to.deep.equal([
-            { data: { num: '4', ref: null, custom: 'abc' }, children: [
+            {
+                data: { num: '4', ref: null, custom: 'abc' }, children: [
                     { data: { num: '31', ref: '4', custom: '12' }, children: [] },
-                ] },
-            { data: { num: '418', ref: null, custom: 'ü' }, children: [
+                ],
+            },
+            {
+                data: { num: '418', ref: null, custom: 'ü' }, children: [
                     { data: { num: '1941', ref: '418', custom: 'de' }, children: [] },
                     { data: { num: '1', ref: '418', custom: 'ZZZz' }, children: [] },
-                ] },
+                ],
+            },
+        ]);
+    });
+    it('should ignore objects if parentId does not exist', function () {
+        chai_1.expect(arrayToTree_1.arrayToTree([
+            { id: '4', parentId: null, custom: 'abc' },
+            { id: '31', parentId: '4', custom: '12' },
+            { id: '1941', parentId: '418', custom: 'de' },
+            { id: '1', parentId: '418', custom: 'ZZZz' },
+            { id: '418', parentId: null, custom: 'ü' },
+            { id: '1313', parentId: '13', custom: 'Not existing' },
+        ])).to.deep.equal([
+            {
+                data: { id: '4', parentId: null, custom: 'abc' }, children: [
+                    { data: { id: '31', parentId: '4', custom: '12' }, children: [] },
+                ],
+            },
+            {
+                data: { id: '418', parentId: null, custom: 'ü' }, children: [
+                    { data: { id: '1941', parentId: '418', custom: 'de' }, children: [] },
+                    { data: { id: '1', parentId: '418', custom: 'ZZZz' }, children: [] },
+                ],
+            },
         ]);
     });
     it('should work with empty inputs', function () {
