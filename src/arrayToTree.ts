bb2675c58cf2922fcc1d7597fa50cce2cfb6c99a
Philip Stanislaus <6912756+philipstanislaus@users.noreply.github.com>
2021-02-03 00:00:34 +0100

    Replace standard code style with prettier

--- src/arrayToTree.ts
+++ src/arrayToTree.ts
@@ -1,116 +1,133 @@
 export interface Item {
-  id?: string | number
-  parentId?: string | number | null
-  [key: string]: any
+  id?: string | number;
+  parentId?: string | number | null;
+  [key: string]: any;
 }
 
 export interface TreeItem {
-  id?: string | number
-  parentId?: string | number | null
-  [key: string]: Item | TreeItem[] | any
+  id?: string | number;
+  parentId?: string | number | null;
+  [key: string]: Item | TreeItem[] | any;
 }
 
 export interface Config {
-  id: string,
-  parentId: string,
-  dataField: string | null,
-  childrenField: string,
-  throwIfOrphans: boolean,
-  rootParentIds: { [rootParentId: string]: true }, // use an object here for fast lookups
+  id: string;
+  parentId: string;
+  dataField: string | null;
+  childrenField: string;
+  throwIfOrphans: boolean;
+  rootParentIds: { [rootParentId: string]: true }; // use an object here for fast lookups
 }
 
 const defaultConfig: Config = {
-  id: 'id',
-  parentId: 'parentId',
-  dataField: 'data',
-  childrenField: 'children',
+  id: "id",
+  parentId: "parentId",
+  dataField: "data",
+  childrenField: "children",
   throwIfOrphans: false,
-  rootParentIds: { '': true },
-}
+  rootParentIds: { "": true },
+};
 
 /**
  * Unflattens an array to a tree with runtime O(n)
  */
-export function arrayToTree (items: Item[], config: Partial<Config> = {}): TreeItem[] {
-  const conf: Config = { ...defaultConfig, ...config }
+export function arrayToTree(
+  items: Item[],
+  config: Partial<Config> = {}
+): TreeItem[] {
+  const conf: Config = { ...defaultConfig, ...config };
 
-	// the resulting unflattened tree
-  const rootItems: TreeItem[] = []
+  // the resulting unflattened tree
+  const rootItems: TreeItem[] = [];
 
-	// stores all already processed items with their ids as key so we can easily look them up
-  const lookup: { [id: string]: TreeItem } = {}
+  // stores all already processed items with their ids as key so we can easily look them up
+  const lookup: { [id: string]: TreeItem } = {};
 
-	// stores all item ids that have not been added to the resulting unflattened tree yet
-	// this is an opt-in property, since it has a slight runtime overhead
-  const orphanIds: null | Set<string | number> = config.throwIfOrphans ? new Set() : null
+  // stores all item ids that have not been added to the resulting unflattened tree yet
+  // this is an opt-in property, since it has a slight runtime overhead
+  const orphanIds: null | Set<string | number> = config.throwIfOrphans
+    ? new Set()
+    : null;
 
-	// idea of this loop:
-	// whenever an item has a parent, but the parent is not yet in the lookup object, we store a preliminary parent
-	// in the lookup object and fill it with the data of the parent later
-	// if an item has no parentId, add it as a root element to rootItems
+  // idea of this loop:
+  // whenever an item has a parent, but the parent is not yet in the lookup object, we store a preliminary parent
+  // in the lookup object and fill it with the data of the parent later
+  // if an item has no parentId, add it as a root element to rootItems
   for (const item of items) {
-    const itemId = getNestedProperty(item, conf.id)
-    const parentId = getNestedProperty(item, conf.parentId)
+    const itemId = getNestedProperty(item, conf.id);
+    const parentId = getNestedProperty(item, conf.parentId);
 
     if (conf.rootParentIds[itemId]) {
-      throw new Error(`The item array contains a node whose parentId both exists in another node and is in ` +
-      `\`rootParentIds\` (\`itemId\`: "${itemId}", \`rootParentIds\`: ${
-        Object.keys(conf.rootParentIds).map(r => `"${r}"`).join(', ')}).`)
+      throw new Error(
+        `The item array contains a node whose parentId both exists in another node and is in ` +
+          `\`rootParentIds\` (\`itemId\`: "${itemId}", \`rootParentIds\`: ${Object.keys(
+            conf.rootParentIds
+          )
+            .map((r) => `"${r}"`)
+            .join(", ")}).`
+      );
     }
 
     // look whether item already exists in the lookup table
     if (!Object.prototype.hasOwnProperty.call(lookup, itemId)) {
-			// item is not yet there, so add a preliminary item (its data will be added later)
-      lookup[itemId] = { [conf.childrenField]: [] }
+      // item is not yet there, so add a preliminary item (its data will be added later)
+      lookup[itemId] = { [conf.childrenField]: [] };
     }
 
-		// if we track orphans, delete this item from the orphan set if it is in it
+    // if we track orphans, delete this item from the orphan set if it is in it
     if (orphanIds) {
-      orphanIds.delete(itemId)
+      orphanIds.delete(itemId);
     }
 
-		// add the current item's data to the item in the lookup table
+    // add the current item's data to the item in the lookup table
     if (conf.dataField) {
-      lookup[itemId][conf.dataField] = item
+      lookup[itemId][conf.dataField] = item;
     } else {
-      lookup[itemId] = { ...item, [conf.childrenField]: lookup[itemId][conf.childrenField] }
+      lookup[itemId] = {
+        ...item,
+        [conf.childrenField]: lookup[itemId][conf.childrenField],
+      };
     }
 
-    const TreeItem = lookup[itemId]
+    const treeItem = lookup[itemId];
 
-    if (parentId === null || parentId === undefined || conf.rootParentIds[parentId]) {
+    if (
+      parentId === null ||
+      parentId === undefined ||
+      conf.rootParentIds[parentId]
+    ) {
       // is a root item
-      rootItems.push(TreeItem)
+      rootItems.push(treeItem);
     } else {
-			// has a parent
+      // has a parent
 
-			// look whether the parent already exists in the lookup table
+      // look whether the parent already exists in the lookup table
       if (!Object.prototype.hasOwnProperty.call(lookup, parentId)) {
-				// parent is not yet there, so add a preliminary parent (its data will be added later)
-        lookup[parentId] = { [conf.childrenField]: [] }
+        // parent is not yet there, so add a preliminary parent (its data will be added later)
+        lookup[parentId] = { [conf.childrenField]: [] };
 
-				// if we track orphans, add the generated parent to the orphan list
+        // if we track orphans, add the generated parent to the orphan list
         if (orphanIds) {
-          orphanIds.add(parentId)
+          orphanIds.add(parentId);
         }
       }
 
-			// add the current item to the parent
-      lookup[parentId][conf.childrenField].push(TreeItem)
+      // add the current item to the parent
+      lookup[parentId][conf.childrenField].push(treeItem);
     }
   }
 
   if (orphanIds?.size) {
     throw new Error(
-			`The items array contains orphans that point to the following parentIds: ` +
-				`[${Array.from(
-					orphanIds,
-				)}]. These parentIds do not exist in the items array. Hint: prevent orphans to result ` +
-				`in an error by passing the following option: { throwIfOrphans: false }`,
-		)
+      `The items array contains orphans that point to the following parentIds: ` +
+        `[${Array.from(
+          orphanIds
+        )}]. These parentIds do not exist in the items array. Hint: prevent orphans to result ` +
+        `in an error by passing the following option: { throwIfOrphans: false }`
+    );
   }
 
-  return rootItems
+  return rootItems;
 }
 
 /**
@@ -120,6 +137,6 @@ export function arrayToTree (items: Item[], config: Partial<Config> = {}): TreeI
  * @param item
  * @param nestedProperty the chained properties to access the nested property. Eg: 'your.nested.property'
  */
-function getNestedProperty (item: Item, nestedProperty: string) {
-  return nestedProperty.split('.').reduce((o, i) => o[i], item)
+function getNestedProperty(item: Item, nestedProperty: string) {
+  return nestedProperty.split(".").reduce((o, i) => o[i], item);
 }
